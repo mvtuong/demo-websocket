a8cc0329fa7e80dbc39fecee2bb8055d22b03d31
mai-cliqz <mai@cliqz.com>
2018-04-22 12:25:39 +0200

    Add logger and create datasource.js, routes.js

--- server.js
+++ server.js
@@ -9,18 +9,12 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var dotenv = require('dotenv');
 var exphbs = require('express-handlebars');
-var mongoose = require('mongoose');
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var i18n = require('i18n');
 
 // Load environment variables from .env file
 dotenv.load();
-
-// Controllers
-var HomeController = require('./controllers/home');
-var userController = require('./controllers/user');
-var contactController = require('./controllers/contact');
 
 // Passport OAuth strategies
 require('./config/passport');
@@ -41,13 +35,6 @@ i18n.configure({
 
 
 var app = express();
-
-
-mongoose.connect(process.env.MONGODB);
-mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
 
 var hbs = exphbs.create({
   defaultLayout: 'main',
@@ -91,23 +78,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(i18n.init);
 
-app.get('/', HomeController.index);
-app.get('/contact', contactController.contactGet);
-app.post('/contact', contactController.contactPost);
-app.get('/account', userController.ensureAuthenticated, userController.accountGet);
-app.put('/account', userController.ensureAuthenticated, userController.accountPut);
-app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
-app.get('/signup', userController.signupGet);
-app.post('/signup', userController.signupPost);
-app.get('/login', userController.loginGet);
-app.post('/login', userController.loginPost);
-app.get('/forgot', userController.forgotGet);
-app.post('/forgot', userController.forgotPost);
-app.get('/reset/:token', userController.resetGet);
-app.post('/reset/:token', userController.resetPost);
-app.get('/logout', userController.logout);
-app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
-
+require('./routes.js')(app);
 // Production error handler
 if (app.get('env') === 'production') {
   app.use(function(err, req, res, next) {
