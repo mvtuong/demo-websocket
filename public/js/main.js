07e17bde5eafff25c5f9021ada7512ea4de397c8
Vinh-Tuong MAI <mvtuong90@gmail.com>
2018-04-23 16:04:07 +0200

    Add websocket interactions (#3)

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
 $(function() {
-  // Your custom JavaScript goes here
+	$('#example').DataTable();
+
+  // WebSocket
+  var ws = new WebSocket('ws://localhost:3000');
+  // event emmited when connected
+  ws.onopen = function () {
+    console.log('websocket is connected ...');
+    // sending a send event to websocket server
+    ws.send('Hello from client!');
+  }
+  // event emmited when receiving message 
+  ws.onmessage = function (ev) {
+    // console.log('Socket data: ', ev.data);
+
+    if (!isNaN(ev.data)) {
+      $('#test-Socket').text(ev.data);
+    }
+  }
+  // Handle any error that occurs.
+  ws.onerror = function(error) {
+    console.error('WebSocket Error: ' + error);
+  };
+
+  // Close if the connection is open.
+  if (0 && ws.readyState === WebSocket.OPEN) {
+    ws.close();
+  }
 });
